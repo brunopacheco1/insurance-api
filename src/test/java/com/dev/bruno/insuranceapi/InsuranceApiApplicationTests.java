c0f2d81251c6382a8772faa92d6f40ac2d1b950e
Bruno da Silva <brunopacheco1@yahoo.com>
2019-03-03 11:07:45 +0100

    Initializing repo.

---
+++ src/test/java/com/dev/bruno/insuranceapi/InsuranceApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dev.bruno.insuranceapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InsuranceApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
